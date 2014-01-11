7236b7ea5895cb0ab78b80271c1dd69e093791d6
Tom Hanlon <tomhanlon@gmail.com>
2014-01-11 14:57:53 -0600

    Stylize navbar and homepage

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(:default, Rails.env)
 
 module Carewell
   class Application < Rails::Application
+
+  # add app/assets/fonts to the asset path
+  config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
