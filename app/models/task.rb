c5bb667dffd19650c21aa4d620fc98057a02e9fd
Eric Boggs <eric.boggs@ac4d.com>
2014-01-11 12:28:08 -0600

    modified models for has_many relationships.  Created task index page and task new page

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,11 @@ class Task < ActiveRecord::Base
   belongs_to :location
 
   validates :category, :inclusion => ['getting places', 'around the home', 'shopping', 'meals', 'personal care', 'odds & ends', 'visits & outings']
+
+  # potentially create a model method to return all tasks here, to be called in view.
+  #concerned this may be a group model method, however.
+  # def find_group_tasks
+  #   Task.joins(:user).where(:users => {group_id: 1})
+  # end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :group
+
+  has_many :tasks
+  has_many :updates
+
 end
