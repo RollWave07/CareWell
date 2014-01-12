24e0b2864228185e6437498e6530fb39acab1ca7
Tom Hanlon <tomhanlon@gmail.com>
2014-01-11 20:07:30 -0600

    Add working Updates#index. Still need to stylize

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,7 +1,10 @@
 class UpdatesController < ApplicationController
   before_action :authenticate_user!
   def index
-    @updates = Update.all
+    @updates = current_user.updates
+    @commenter_name = current_user.first_name
+    @commenter_picture = current_user.picture
+    @group_name = Group.find(current_user.group_id).name
   end
 
   def show
