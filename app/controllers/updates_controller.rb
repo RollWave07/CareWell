5ab2aed12816bab9c04f3bf1c8f737489b5959a8
Tom Hanlon <tomhanlon@gmail.com>
2014-01-11 22:42:27 -0600

    Make progress on controller. Still need create and update functionality.

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,5 +1,7 @@
 class UpdatesController < ApplicationController
   before_action :authenticate_user!
+  before_filter :find_group
+
   def index
     @updates = current_user.updates
     @commenter_name = current_user.first_name
@@ -11,8 +13,41 @@ class UpdatesController < ApplicationController
   end
 
   def edit
+    @update = Update.find(params[:id])
   end
 
+  # def create
+  #   @group = Group.new(group_params)
+  #   current_user.group = @group
+  #   respond_to do |format|
+  #     if @group.save
+  #       format.html { redirect_to group_tasks_path(@group), notice: 'group was successfully created.' }
+  #       format.js { render layout: false }
+  #     else
+  #       format.html { render action: 'new' }
+  #     end
+  #   end
+  # end
+
+  # def update
+  #   respond_to do |format|
+  #     if @group.update(group_params)
+  #       format.html { redirect_to @group, notice: 'group was successfully updated.' }
+  #     else
+  #       format.html { render action: 'edit' }
+  #     end
+  #   end
+  # end
+
   def new
+  end
+
+  private
+  def find_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def update_params
+      params.require(:update).permit(:comment, :picture, :task_id, :user_id)
   end
 end
