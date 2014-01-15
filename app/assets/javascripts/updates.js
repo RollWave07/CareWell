330dd79a794885793262a7b19c280fcc0e48ef85
Brian Sisney <briansisney@gmail.com>
2014-01-14 19:02:26 -0600

    comments clear after ajax submit

--- app/assets/javascripts/updates.js
+++ app/assets/javascripts/updates.js
@@ -2,4 +2,8 @@
 //   console.log("Ajax Response data:", data);
 // });
 
-
+// $(".add_update").on("click", function (e) {
+//   e.preventDefault();
+//   console.log("click");
+//   $(".update_comment").text("");
+// });
