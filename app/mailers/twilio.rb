50df907f14dd49ed91de24d23de4b4c3ce1941e1
Brian Sisney <briansisney@gmail.com>
2014-01-15 16:40:10 -0600

    took out twilio.rb page for heroku test

--- app/mailers/twilio.rb
+++ app/mailers/twilio.rb
@@ -5,14 +5,14 @@ require 'twilio-ruby'
 
 # def Twiliotext
    #Get your Account Sid and Auth Token from twilio.com/user/account
-  account_sid = ENV['TWILIO_ACCOUNT_SID']
-  auth_token = ENV['TWILIO_ACCOUNT_TOKEN']
-  @client = Twilio::REST::Client.new account_sid, auth_token
+  # account_sid = ENV['TWILIO_ACCOUNT_SID']
+  # auth_token = ENV['TWILIO_ACCOUNT_TOKEN']
+  # @client = Twilio::REST::Client.new account_sid, auth_token
   
-  message = @client.account.sms.messages.create(
-    :to => "6175158907",  
-    :from => "8588668901",
-    :body => "Jenny please?! I love you hooo!! <3"
-  )
+  # message = @client.account.sms.messages.create(
+  #   :to => "6175158907",  
+  #   :from => "8588668901",
+  #   :body => "Jenny please?! I love you hooo!! <3"
+  # )
   # puts message.sid
 # end
