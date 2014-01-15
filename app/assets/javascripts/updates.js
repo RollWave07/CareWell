61ecc88e8c062240f88683ce470b7f207e6af2e8
Brian Sisney <briansisney@gmail.com>
2014-01-14 18:34:51 -0600

    added delete method for comments

--- app/assets/javascripts/updates.js
+++ app/assets/javascripts/updates.js
@@ -2,9 +2,4 @@
 //   console.log("Ajax Response data:", data);
 // });
 
-// num = 0
-// $(".like-button").on("click", function (e) {
-//   e.preventDefault()
-//   num += 1;
-//   $(".like-counter").text("Likes: "+ num);
-// });
+
