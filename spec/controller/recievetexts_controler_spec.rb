7e5e0ccbc44993b5866c6f11234540f9d44e4097
Brian Sisney <briansisney@gmail.com>
2014-01-21 09:55:33 -0600

    stub in rspec

--- spec/controller/recievetexts_controler_spec.rb
+++ spec/controller/recievetexts_controler_spec.rb
@@ -3,36 +3,37 @@ require "spec_helper"
 describe RecievetextsController do
   describe "Post#index" do
     it "assignes the @task to the correct task when a task is available" do
-      @comment = 
-      @assignee = 
-      @task = 
+      comment.stub(:body).with('a string')
+      expect comment.class.to eq(String)
+      # @assignee = 
+      # @task = 
 
 
     end
 
-    it "creates a new task when a task is not available" do
-      params["Body"] = 
+    # it "creates a new task when a task is not available" do
+    #   params["Body"] = 
 
 
 
-    end
+    # end
 
   end
 end
 
 
-    @comment = params["Body"]
-    @assignee = User.where(phone: params["From"].gsub('+1','')).first
-    @task = Task.where(assignee_id: @assignee).completed_recently.first
+  #   @comment = params["Body"]
+  #   @assignee = User.where(phone: params["From"].gsub('+1','')).first
+  #   @task = Task.where(assignee_id: @assignee).completed_recently.first
     
-    unless @task
-        @task = Task.create(user_id: @assignee, assignee_id: @assignee, title: "Update")
-    end 
+  #   unless @task
+  #       @task = Task.create(user_id: @assignee, assignee_id: @assignee, title: "Update")
+  #   end 
 
-    # logger.info "+++task=#{@task.inspect}"
+  #   # logger.info "+++task=#{@task.inspect}"
 
-    # user_id =
-    #SMSLogger.log_text_message from_number, message_bodyme
-    #params.require(:update).permit(:comment, :picture, :task_id, :user_id)
-    Update.create(user: @assignee, task:@task, comment:@comment)
-  end
+  #   # user_id =
+  #   #SMSLogger.log_text_message from_number, message_bodyme
+  #   #params.require(:update).permit(:comment, :picture, :task_id, :user_id)
+  #   Update.create(user: @assignee, task:@task, comment:@comment)
+  # end
