ac2c066f5a87e089c2df3dd3886e62709a33fe19
Eric Boggs <eric.boggs@ac4d.com>
2014-01-22 12:07:29 -0600

    figured out the invite, just need to add organizer

--- app/mailers/mailer_invitation.rb
+++ app/mailers/mailer_invitation.rb
@@ -1,41 +1,30 @@
 class MailerInvitation < ActionMailer::Base
-  default from: "hello@carewelldesign.com"
 
-  def event(task, group_id)
-  # assignee information (from task)
-    @first_name = task.assignee.first_name
-    @last_name = task.assignee.last_name
-    @email  = task.assignee.email
-  #task information
-    @task = task
-    @task_title = task.title
-    @task_category = task.category
-    @task_information = task.information
-    @task_date = task.task_date.strftime('%A, %B %e, %Y')
-  #group information
-    @group = group_id
-  #email content/body information
-    @subject_line = "New CareWell Task: #{@task_title}"
-    @greeting = "Hi " + task.assignee.first_name + ','
-    attachments['event.ics'] = {:mime_type => 'text/calendar', :content => create_ical }
-    mail( to: @email, subject: @subject_line)
-  end
+  require 'icalendar'
+  require 'date'
 
-  def create_ical
-     ical = Icalendar::Calendar.new
-     e = Icalendar::Event.new
-     e.start = (@task.start_time).utc
-     e.start.icalendar_tzid="UTC" # set timezone as "UTC"
-     e.end = (@task.start_time + @task.duration.to_i.minutes).utc
-     e.end.icalendar_tzid="UTC"
-     e.organizer "#{@task.user.first_name}"
-     #might need uid but maybe not.  test.
-     e.uid "CW Task #{@task.id}"
-     e.summary @subject_line
-     e.description "@task.information"
-     ical.add_event(e)
-     ical.publish
-     ical.to_ical
-  end
+   def test_ical2(task)
+       @subject_line = "New CareWell Task: #{task.title}"
+       @organizer = task.user.first_name
+        mail(:to => task.assignee.email, :subject => @subject_line,
+                     :from => "hello@carewelldesign.com") do |format|
+          format.ics {
+                 ical = Icalendar::Calendar.new
+                 e = Icalendar::Event.new
+                 e.start = task.start_time.to_datetime.utc
+                 e.start.icalendar_tzid="UTC" # set timezone as "UTC"
+                 e.end = (task.start_time + task.duration.to_i.minutes).to_datetime.utc
+                 e.end.icalendar_tzid="UTC"
+                 e.organizer "CareWell"
+                 e.uid "CW #{task.id}"
+                 e.summary "#{task.title}"
+                 e.description "#{task.information}"
+                 ical.add_event(e)
+                 ical.publish
+                 ical.to_ical
+                 render :text => ical.to_ical, :layout => false
+                }
+       end
+     end
 
 end
