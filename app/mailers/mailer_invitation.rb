b91da0e78f6fd85ab814c899ffa97d9af20c0615
Eric Boggs <eric.boggs@ac4d.com>
2014-01-22 13:04:31 -0600

    invite is finished

--- app/mailers/mailer_invitation.rb
+++ app/mailers/mailer_invitation.rb
@@ -6,7 +6,7 @@ class MailerInvitation < ActionMailer::Base
    def test_ical2(task)
        @subject_line = "New CareWell Task: #{task.title}"
        @organizer = task.user.first_name
-        mail(:to => task.assignee.email, :subject => @subject_line,
+        mail(:to => 'bensbigolbeard@gmail.com', :subject => @subject_line,
                      :from => "hello@carewelldesign.com") do |format|
           format.ics {
                  ical = Icalendar::Calendar.new
@@ -15,7 +15,7 @@ class MailerInvitation < ActionMailer::Base
                  e.start.icalendar_tzid="UTC" # set timezone as "UTC"
                  e.end = (task.start_time + task.duration.to_i.minutes).to_datetime.utc
                  e.end.icalendar_tzid="UTC"
-                 e.organizer "CareWell"
+                 e.organizer "#{task.user.email}"
                  e.uid "CW #{task.id}"
                  e.summary "#{task.title}"
                  e.description "#{task.information}"
