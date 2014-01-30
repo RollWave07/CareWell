2a894af3695f3314cbe219103effb17edde42327
Eric Boggs <eric.boggs@ac4d.com>
2014-01-30 10:28:12 -0600

    fixed the updates

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,6 +32,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
+  version :sm_thumb do
+    process :resize_to_fill => [50,50]
+  end
   version :thumb do
     process :resize_to_fill => [50, 75]
   end
