839346cf33329786718b48168dab880fdc3c91da
GeneChatham <genechatham@gmail.com>
2014-02-09 22:20:53 -0600

    Remove need to enter password twice, show password as typed.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -9,7 +9,7 @@ $('.close-window').on('click', function() {
 // ajax request to get list of already taken emails
 $('input#user_email').on('blur', function() {
   console.log("blur");
-  emailData = this.value;
+  var emailData = this.value;
   console.log(this);
   console.log("emailData:" + emailData);
   $.ajax({
@@ -21,7 +21,14 @@ $('input#user_email').on('blur', function() {
         console.log(data.available)
     }  
   });      
+});
 
-
+// get value from single password field and add it to 
+// confirm password field
+$('.btn-sign-up').on('click', function(e) {
+  var password = $('.password input').val();
+  // console.log(password);
+  $('.confirm-password input').val(password);
+  // console.log($('.confirm-password input').val());
 
 });
