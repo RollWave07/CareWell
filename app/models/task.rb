689be2ce5b2e59b91ce8f775fcc83bdb525afa34
Eric Boggs <eric.boggs@ac4d.com>
2014-03-04 17:38:08 -0500

    can assign task to oneself

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,7 +6,7 @@ class Task < ActiveRecord::Base
   belongs_to :location
   has_many :updates
 
-  
+
   validates :title, presence: true
 
 
@@ -16,12 +16,12 @@ class Task < ActiveRecord::Base
   #___GRAPH DATA
   scope :future, lambda {where(["task_date > ?", Time.now])}
   scope :past, lambda {where(["task_date < ?", Time.now])}
-  
+
   scope :week_one, lambda {where(task_date:(7.day.ago..Time.now))}
   scope :week_two, lambda {where(task_date:(14.day.ago..8.day.ago))}
   scope :week_three, lambda {where(task_date:(21.day.ago..15.day.ago))}
   scope :week_four, lambda {where(task_date:(28.day.ago..22.day.ago))}
-  
+
   @scopes_period = ["week_one", "week_two", "week_three", "week_four"]
 
   def self.count_per_period(tasks, time_period = @scopes_period)
@@ -114,98 +114,5 @@ class Task < ActiveRecord::Base
   def self.average_duration_per_category(tasks)
     tasks.group(:category).select(:category, :duration).average(:duration)
   end
-
-
-
-  ## potential model method
-  # def needs_assignee
-  #   if self.assignee_id.nil? && self.user_id == current_user.id
-  #   return "Invite A Helper"
-  #   elsif self.assignee_id.nil?
-  #   return "I can do this!"
-  #   else
-  #   return "This is assigned to you."
-  #   end
-  # end
-
-
-
-  
-
-
-
-
-  # def self.top_three_members(tasks)
-  #   top_three(sort_by_value(format_name(tasks_per_member(tasks))))
-  # end
-  #INFO: does not work but might be a good start to refactor
-  # def self.tasks_per_criteria(tasks, criteria)
-  #   memberstats = {}
-  #   tasks.each do |task|
-  #     if memberstats[task.criteria]
-  #       memberstats[task.criteria] += 1
-  #     else
-  #       memberstats[task.criteria] = 1
-  #     end
-  #   end
-  #   memberstats
-  # end
-
-
-
-  #OLD SORTING METHOD:
-  #UNTESTED CHANGES took out assignee
-  # def self.tasks_per_member(tasks)
-  #   memberstats = {}
-  #   tasks.each do |task|
-  #     if memberstats[task.assignee]
-  #       memberstats[task] += 1
-  #     else
-  #       memberstats[task] = 1
-  #     end
-  #   end
-  #   memberstats
-  # end
-
-  # #UNTESTED CHANGES added assignee
-  # def self.format_name(hash)
-  #   hash.map { |k, v| k="#{k.assignee.first_name} #{k.assignee.last_name}", v }
-  # end
-
-  # def self.format_date(date)
-  #   date.strftime("%a, %b %d")
-  # end
-
-  # def self.sort_by_value(hash)
-  #   hash.sort_by { |a, b| b }
-  # end
-
-  # def self.top_three(value)
-  #   i=0
-  #   the_top_three = []
-  #   while i<3
-  #     the_top_three << value[i] if value[i]
-  #     i+=1
-  #   end
-  #   the_top_three
-  # end
-
-
-  #OLD STUFF --------------
-  # validates :category, :inclusion => ['getting places', 'around the home', 'shopping', 'meals', 'personal care', 'odds & ends', 'visits & outings']
-  # scope :last_month, lambda {where(task_date:(31.day.ago..Time.now))}
-  # scope :last_week, lambda {where(task_date:(7.day.ago..Time.now))}
-  # scope :next_month, lambda {where(task_date:(Time.now..Time.now+31.day))}
-  # scope :next_week, lambda {where(task_date:(Time.now..Time.now+7.day))}
-  # scope :all_time, lambda {where(task_date:(Time.new(2014, 1, 1)..Time.now))}
-
-  # potentially create a model method to return all tasks here, to be called in view.
-  #concerned this may be a group model method, however.
-  # def find_group_tasks
-  #   Task.joins(:user).where(:users => {group_id: 1})
-  # end
-
-
-
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
 
   # scope :future, lambda {where(["task_date > ?", Time.now])}
   # scope :past, lambda {where(["task_date < ?", Time.now])}
-  
+
   has_many :tasks
   has_many :updates
 
@@ -17,68 +17,5 @@ class User < ActiveRecord::Base
   def self.users_in_group(group)
     self.where(group_id: group.id)
   end
-
-
-  # TODO:
-  # FIXME:
-  #   Does not actually limit to tasks which have been completed.
-  # def tasks_completed(params = {})
-  #   find_params = { assignee_id: id }
-  #   find_params[:task_date] = params[:since]..Time.now if params[:since]
-
-  #   Task.where(find_params).count
-  # end
-
-  
-  # my_user.tasks_completed(task_date: 1.month.ago..Time.now)
-  # my_user.tasks_completed(since: 1.month.ago)
-
-  # #top users last week:
-  # def self.users_with_tasks_last_month
-  #   #this is for the user_id and I want assignee_id
-  #   binding.pry
-  #   self.joins(:tasks, foreign_key: :assignee_id).where(tasks: {task_date:(31.day.ago..Time.now)})
-  # end
-
-  # def self.find_assignee_with_tasks_last_month
-  #   self.includes(:tasks).where(tasks: {task_date:(31.day.ago..Time.now)})
-  # end
-
-
-
-
-  # def self.tasks_per_member(tasks)
-  #   memberstats = {}
-  #   tasks.each do |task|
-  #     if memberstats[task.assignee]
-  #       memberstats[task.assignee] += 1
-  #     else
-  #       memberstats[task.assignee] = 1
-  #     end
-  #   end
-  #   memberstats
-  # end
-
-  # def self.format_name(hash)
-  #   hash.map { |k, v| k="#{k.first_name} #{k.last_name}", v }
-  # end
-
-  # def self.format_date(date)
-  #   date.strftime("%a, %b %d")
-  # end
-
-  # def self.sort_by_value(hash)
-  #   hash.sort_by { |a, b| b }
-  # end
-
-  # def self.top_three(value)
-  #   i=0
-  #   the_top_three = []
-  #   while i<3
-  #     the_top_three << value[i] if value[i]
-  #     i+=1
-  #   end
-  #   the_top_three
-  # end
 
 end
