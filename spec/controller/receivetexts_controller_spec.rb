6e37f69f38a4cd735a95fb6e2dae4fd517ae8f3c
Brian Sisney <briansisney@gmail.com>
2014-03-08 12:35:47 -0500

    megred with master, made small notes on what to test

--- spec/controller/receivetexts_controller_spec.rb
+++ spec/controller/receivetexts_controller_spec.rb
@@ -1,55 +1,55 @@
-# require "spec_helper"
-# require "pry"
+require "spec_helper"
+require "pry"
 
-# describe ReceivetextsController, type: :controller do
-#   let(:params) { {"Body" => "test body", "From" => "6175158907"} }
-#   let(:task) {Task.new(title: "test task")}
-#   let(:current_user) {User.new}
+describe ReceivetextsController, type: :controller do
+  let(:params) { {"Body" => "test body", "From" => "6175158907"} }
+  let(:task) {Task.new(title: "test task")}
+  let(:current_user) {User.new}
 
-#   describe "Receivetexts#index" do
-#     it "assignes the @task to the correct task when a task is available" do
-#       # comment.stub(:body).with('a string')
-#       # expect comment.class.to eq(String)
-#       # @assignee = 
-#       # @task = 
-#       Task.stub_chain(:where, :completed_recently, :first).and_return(task)
-#       binding.pry
+  describe "Receivetexts#index" do
+    it "assignes the @task to the correct task when a task is available" do
+      # comment.stub(:body).with('a string')
+      # expect comment.class.to eq(String)
+      # @assignee = 
+      # @task = 
+      Task.stub_chain(:where, :completed_recently, :first).and_return(task)
+      binding.pry
 
-#       get :index
+      get :index
 
-#       expect(assigns(:task)).to eq(task)
-
-
-
-
-#     end
-
-#     # it "creates a new task when a task is not available" do
-#     #   params["Body"] = 
+      expect(assigns(:task)).to eq(task)
 
 
 
-#     # end
 
-#   end
-# end
+    end
 
-  # @task can be nil
-  #   if so, @task will be created using @assignee
+    # it "creates a new task when a task is not available" do
+    #   params["Body"] = 
 
 
-  #   @comment = params["Body"]
-  #   @assignee = User.where(phone: params["From"].gsub('+1','')).first
-  #   @task = Task.where(assignee_id: @assignee).completed_recently.first
+
+    # end
+
+  end
+end
+
+  @task can be nil
+    if so, @task will be created using @assignee
+
+
+    @comment = params["Body"]
+    @assignee = User.where(phone: params["From"].gsub('+1','')).first
+    @task = Task.where(assignee_id: @assignee).completed_recently.first
     
-  #   unless @task
-  #       @task = Task.create(user_id: @assignee, assignee_id: @assignee, title: "Update")
-  #   end 
+    unless @task
+        @task = Task.create(user_id: @assignee, assignee_id: @assignee, title: "Update")
+    end 
 
-  #   # logger.info "+++task=#{@task.inspect}"
+    # logger.info "+++task=#{@task.inspect}"
 
-  #   # user_id =
-  #   #SMSLogger.log_text_message from_number, message_bodyme
-  #   #params.require(:update).permit(:comment, :picture, :task_id, :user_id)
-  #   Update.create(user: @assignee, task:@task, comment:@comment)
-  # end
+    # user_id =
+    #SMSLogger.log_text_message from_number, message_bodyme
+    #params.require(:update).permit(:comment, :picture, :task_id, :user_id)
+    Update.create(user: @assignee, task:@task, comment:@comment)
+  end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -4,4 +4,17 @@ class TaskTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+  #Things to test:
+  #scopes are working
+
+  #def self.count_per_period is expags an array and the correct numbers in the array
+
+
+
+  #likes count is correct
+
+
+  #duration_total_past_week(tasks)
 end
