aa7c4f8b965c9bdaf99661c51893ea134fa648fd
Brian Sisney <briansisney@gmail.com>
2014-03-14 16:50:56 -0400

    tests for receives text controller and task model

--- spec/controller/receivetexts_controller_spec.rb
+++ spec/controller/receivetexts_controller_spec.rb
@@ -3,32 +3,58 @@ require "pry"
 
 describe ReceivetextsController, type: :controller do
   let(:user) do
-    User.create(first_name: "Captain",last_name: "Hook", email: "captain@hook.com", password:"password", phone: 6175158907, group_id: 1, admin: true )
+    User.create!([{first_name: "Captain",last_name: "Hook", email: "captain@hook.com", password:"password", phone: 6175158907, group_id: 1, admin: true},
+      {first_name: "Tiger",last_name: "Lily", email: "tiger@lily.com", password:"password", phone: 6175158907, group_id: 1, admin: false }
+     ])
     
   end
   let(:task) do
-    Task.create(title: "Sample Task", groupid: 1, category: "getting places", duration: 60, task_date: Time.now, assignee_id: 1 , user_id: 1)
+    Task.create!([
+      {title: "Sample Task 2", groupid: 1, category: "getting places", duration: 60, task_date: Time.now+1.day, assignee_id: User.first.id , user_id: User.first.id},
+      {title: "Sample Task 3", groupid: 1, category: "getting places", duration: 60, task_date: Time.now-2.day, assignee_id: User.first.id , user_id: User.first.id},
+      {title: "Sample Task 4", groupid: 1, category: "getting places", duration: 30, task_date: Time.now-1.hour, assignee_id: nil , user_id: User.first.id}])
+
   end
 
-  describe "GET 'index'" do
+  describe "recieves a text" do
     it "returns http success" do
       user
       task
       get 'index', "Body" => "test body", "From" => "6175158907"
       response.should be_success
     end
-    it "creates an update" do
-      user
-      task
-      get 'index', "Body" => "test body", "From" => "6175158907"
-      expect(Update.count).to eq(1)
+    
+    context 'finds a recently completed task' do
+      it "creates an update on an existing task" do
+        user
+        task
+        Task.create(title: "Sample Task 1", groupid: 1, category: "getting places", duration: 30, task_date: Time.now-1.hour, assignee_id: User.first.id , user_id: User.first.id)
+        get 'index', "Body" => "test body", "From" => "6175158907"
+        expect(Update.count).to eq(1)
+        assert_equal Update.first.task.title, "Sample Task 1"
+      end
+      it 'assignes it to the correct user' do
+        user
+        task
+        Task.create(title: "Sample Task 5", groupid: 1, category: "getting places", duration: 30, task_date: Time.now-1.hour, assignee_id: User.last.id , user_id: User.last.id)
+        Task.create(title: "Sample Task 1", groupid: 1, category: "getting places", duration: 30, task_date: Time.now-2.hour, assignee_id: User.first.id , user_id: User.last.id)
+        
+        get 'index', "Body" => "test body", "From" => "6175158907"
+        expect(Update.count).to eq(1)
+        assert_equal Update.first.task.title, "Sample Task 1"
+      end
     end
-    # it "assignes user one to the most recent task" do
-      
-    # end
-    # it "creates a new task when task not found" do
-      
-    # end
+
+    context 'does not find a recent completed task' do
+      it "creates an update on a new task" do
+        user
+        task
+        get 'index', "Body" => "test body", "From" => "6175158907"
+        expect(Update.count).to eq(1)
+        assert_equal Update.first.id, Task.last.updates.take.id
+        assert_equal Task.last.title, "Text Update"
+      end
+    end
   end
 
   
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,33 +4,63 @@ describe Task do
   let(:users) do
     User.create!([
       {first_name: "Tinker",last_name: "Bell", email: "tinker@bell.com", password:"password", phone: 6175158907, group_id: 1, admin: false },
-      {first_name: "Tiger",last_name: "Lily", email: "tiger@lily.com", password:"password", phone: 6175158907, group_id: 1, admin: false }])
+      {first_name: "Tiger",last_name: "Lily", email: "tiger@lily.com", password:"password", phone: 6175158907, group_id: 1, admin: false },
+      {first_name: "Holy",last_name: "Spearit", email: "holy@spearit.com", password:"password", phone: 6175158907, group_id: 2, admin: true }])
   end
   let(:tasks) do
     Task.create!([
-      {title: "Task 1", groupid: 1, category: "getting places", duration: 60, task_date: Time.now+1.day, assignee_id: nil , user_id: User.first.id},
+      {title: "Task 1", groupid: 1, category: "getting places", duration: 30, task_date: Time.now+1.day, assignee_id: nil , user_id: User.first.id},
       {title: "Task 2", groupid: 1, category: "getting places", duration: 60, task_date: Time.now+1.day, assignee_id: User.first.id , user_id: User.first.id},
-      {title: "Task 3", groupid: 1, category: "getting places", duration: 60, task_date: Time.now+1.day, assignee_id: User.find(User.first.id+1).id , user_id: User.first.id},
-      {title: "Task 4", groupid: 1, category: "getting places", duration: 60, task_date: Time.now+1.day, assignee_id: User.last.id , user_id: User.first.id}])
+      {title: "Task 3", groupid: 1, category: "meals", duration: 90, task_date: Time.now+1.day, assignee_id: User.find(User.first.id+1).id , user_id: User.first.id},
+      {title: "Task 4", groupid: 1, category: "odds & ends", duration: 15, task_date: Time.now+1.day, assignee_id: User.find(User.first.id+1).id , user_id: User.first.id},
+      {title: "Task 5", groupid: 2, category: "getting places", duration: 60, task_date: Time.now+1.day, assignee_id: User.last.id , user_id: User.last.id}
+      ])
   end
-  # subject do
-  #   Task.create()
 
-  # end  
-  describe "#upcoming_tasks(group)" do
-    it 'returns four tasks' do
-      users
-      tasks
-      expect(Task.upcoming_tasks(1).count).to eq 4
+  describe 'filters tasks' do |variable|
+    context "into upcoming tasks by group" do
+      it 'returns four tasks' do
+        users
+        tasks
+        expect(Task.upcoming_tasks(1).count).to eq 4
+      end
     end
-    # its(:unassigned) {should = 3}
-  end
-  describe "#unassigned(tasks)" do
-    
-    it 'returns 1 ' do
+    context "into unassigned tasks" do   
+      it 'returns one task' do
+        users
+        tasks
+        expect(Task.unassigned(Task.all).count).to eq 1
+      end
+    end
+    context 'assigns tasks to a user' do
+      it 'returns number of tasks per user' do
+        users
+        tasks
+        assert_equal Task.assigned_to_specific_user(Task.all, User.first).count, 1
+        assert_equal Task.assigned_to_specific_user(Task.all, User.find(User.first.id+1)).count, 2
+      end
+    end
+
+  end  
+  describe 'analyzes data for charts ' do
+    it 'creates a hash with four elements' do
       users
       tasks
-      expect(Task.unassigned(Task.all).count).to eq 1
+      categories = Task.categories_per_month(1)
+      assert_equal categories.class, Hash
+      assert_equal categories.count, 3
+    end
+    it 'converts hash into array of arrays chart' do
+      users
+      tasks
+      categories = Task.categories_per_month(1)
+      chart_array=Task.bar_chart_array(categories)
+      assert_equal chart_array.class, Array
+      assert_equal chart_array.count, 8
+      assert_equal chart_array[0], 2
+      assert_equal chart_array[3], 1
+      assert_equal chart_array[5], 1
+      assert_equal chart_array[1], 0
     end
   end
   
