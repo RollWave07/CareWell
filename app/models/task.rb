9a12dc36be7c4ac48e1c1871ad0d06ea01b8cfc1
Eric Boggs <eric.boggs@ac4d.com>
2014-03-17 11:54:06 -0500

    fixed location db relationship

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ActiveRecord::Base
   belongs_to :user
   # belongs_to :group
   belongs_to :assignee, :class_name => "User"
-  belongs_to :location
+  # belongs_to :location
   has_many :updates
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140127015435) do
+ActiveRecord::Schema.define(version: 20140317164446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20140127015435) do
     t.datetime "updated_at"
     t.integer  "duration"
     t.integer  "groupid"
+    t.string   "location"
   end
 
   add_index "tasks", ["assignee_id"], name: "index_tasks_on_assignee_id", using: :btree
