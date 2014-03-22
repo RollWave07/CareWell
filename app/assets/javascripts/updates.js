75a8806eb1341324ff177ffe781e432b625f918f
Eric Boggs <eric.boggs@ac4d.com>
2014-03-22 13:42:18 -0500

    resized task-box

--- app/assets/javascripts/updates.js
+++ app/assets/javascripts/updates.js
@@ -1,14 +1,4 @@
-// $(document).on("ajax:success", function (e, data) {
-//   console.log("Ajax Response data:", data);
-// });
 
-// $(".add_update").on("click", function (e) {
-//   e.preventDefault();
-//   console.log("click");
-//   $(".update_comment").text("");
-// });
-
-// THIS ONE:
 
 $(document).on("ajax:success", ".like-it-now", function(e, data, status, xhr){
   $("#vote" + data.vote_id).text(data.count);
@@ -16,26 +6,9 @@ $(document).on("ajax:success", ".like-it-now", function(e, data, status, xhr){
   // console.log("#vote" + data.vote_id)
 });
 
-// STOP
-
-// $(".button_to").on("ajax:success", function(e, data, status, xhr){
-//   // console.log(data)
-//   // console.log(status)
-//   $("#vote" + data.vote_id).text(data.count);
-//   // console.log("#vote" + data.vote_id)
-// });)
-
-
-// $(".row").on("ajax:success", function(e, data, status, xhr){
-//   $("#update"+data.update).remove();
-// });
-
-
-// $("body").on("ajax:success", function(e, data, status, xhr){
-//   switch (type){
-//     case "delete":
-
-//     case "vote":
-//   }
-
-// });
+$(document).ready( function() {
+  $('.choose-photo').on('click',function (){
+    console.log("clicked");
+    $('input#update_picture').click();
+  });//end click
+})//end ready
