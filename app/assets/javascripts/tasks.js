1ed133350a792b39140d350d7073d3051975058e
Eric Boggs <eric.boggs@ac4d.com>
2014-03-22 14:22:23 -0500

    slidetoggle for details

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -11,75 +11,9 @@ $(document).ready(function() {
     }
   });//end task form icon wrapper
 
-  //create an array of images that will randomly pick one if there aren't any tasks.
-
-//   var randomPhoto = function () {
-
-//     var randomPhotoIndex, photoArray;
-
-//     randomPhotoIndex = Math.floor(Math.random() * 6);
-//     photoArray = ['a','b','c','d','e'];
-//     // return photoArray[randomPhotoIndex];
-//     return 'pink-flower.png';
-//   };
-
-
-// });//end ready
-
-//autocomplete for task location
-//turn off for now because page movement is annoying
-// $(function() {
-//     var locationTags = [
-//       "Restaurant",
-//       "N/A",
-//       "Home",
-//       "Pharmacy",
-//       "Drs. office",
-//       "Park",
-//       "Friends house",
-//       "Online",
-//       "Remote",
-//       "Outside",
-//       "Book Store",
-//       "Downtown",
-//       "Next door",
-//       "Over the phone",
-//     ];
-//     $( "#task_location" ).autocomplete({
-//         source: locationTags
-//       });
-//     });
-
-//pop up the categories selection div.
-
-// $('.pick-categories').on('click', function(){
-//   $('.categories').fadeToggle();
-// });
-
-//when the icon is chosen (or a dismiss button is hit) then close the box.
-
-// $('.task-form-icon-wrapper').on('click', function() {
-//   replacementText = $(this).find('label').text();
-//   $('.categories').fadeToggle();
-//   $('#task_category').val(replacementText);
-// })
-
-//select a tab test
-
-// $(document).ready(function() {
-//   $('#opentasks').tab('show');
-// });
-
-// $(document).ready( function () {
-//   $('.task-index-header h1').hover(
-//     function() {
-//       console.log('show');
-//       $('.edit-indicator').show();
-//     },
-//     function(){
-//       console.log('hide');
-//       $('.edit-indicator').hide();
-//     });
+  $('.arrow').click( function() {
+    $(this).closest('.task-card').find('.task-details').slideToggle();
+  });
 
 
 $(document).on("ajax:success", ".signup", function(e, data, status, xhr){
